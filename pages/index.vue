dfbadc0f154a0d605ec06ec07113f1bed33c9243
ajh123 <ajh123@users.noreply.github.com>
2024-08-05 16:41:21 +0100

    refactor: become a nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DarkModeToggle from '~/components/DarkModeToggle.vue';
+</script>
+
+<template>
+  <div class="h-screen v-screen">
+    <Button>Click me</Button>
+    <DarkModeToggle/>
+  </div>
+</template>
